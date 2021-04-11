8ee8cda24ebd3c90babe971534bec3190c9c73fd
himeno-yusuke <himeno-yusuke@kayac.com>
2021-04-11 19:25:56 +0900

    jsonを保存するまで

--- 0004-scraping-shops/src/index.ts
+++ 0004-scraping-shops/src/index.ts
@@ -1,15 +1,22 @@
-import puppeteer from "puppeteer-extra"
-import { Page } from "puppeteer-extra/dist/puppeteer"
-import pluginStealth from "puppeteer-extra-plugin-stealth"
+import puppeteer, { Page } from "puppeteer"
 import dayjs from "dayjs"
 import fs from 'fs'
 
-puppeteer.use(pluginStealth())
+const chunk = <T>(list: T[], n: number) => list.reduce<T[][]>((acc, item, i) => {
+  const index = Math.floor(i / n)
+  if(acc[index] === undefined) acc[index] = []
+  acc[index].push(item)
+  return acc
+}, [])
+
+const SITE_URL = "https://tabelog.com"
 
 /**
- * - Google サインイン
+ * サインイン
  */
 const auth = async (page: Page) => {
+
+  await page.goto(SITE_URL)
 
   const navigationPromise = page.waitForNavigation()
 
@@ -21,119 +28,133 @@ const auth = async (page: Page) => {
   const cookie = JSON.parse(content)
 
   await page.setCookie(...cookie)
-  await page.goto("https://tabelog.com/rvwr/010252527/visited_restaurants/list?msu=010252527&review_content_exist=0&PG=1&from_search=&voluntary_search=1&SrtT=lvd&Srt=D&sort_mode=&from_search_form=1&lid=&pcd=0&Cat=MC&LstCat=0&RdoCosTp=2&LstCos=0&LstCosT=0&LstRev=0&sw=&LstKind=01&LstReserve=0&LstSmoking=0")
+
+  await page.reload()
 
   await navigationPromise
 
   return page
 }
 
+/**
+ * 投稿のURLを取得する
+ */
+const urls: string[] = []
+
+const getPosts = async (page: Page): Promise<string[]> => {
+  await page.waitForSelector(".rvw-item .c-rating-v2__val")
+  const $next = (await page.$$('a.c-pagination__arrow[rel="next"]'))
+  const $shops = (await page.$$(".rvw-item"))
+
+  const getPostDetaulUrlPromises = $shops.map(async $shop => {
+    const url = await $shop.$eval(
+      '.p-preview-visit--score',
+      (item: Element) => item.getAttribute('data-detail-url')
+    ).catch((err) => {
+      console.error(err)
+      return null
+    })
+    return url
+  })
+
+  const newUrls = await Promise.all(getPostDetaulUrlPromises)
+
+  if(newUrls.length <= 0) return urls
+
+  urls.push(...newUrls)
+
+  if($next.length <= 0) return urls
+
+  await $next[0].click()
+  return await getPosts(page)
+}
 
 type Shop = {
   name: string
-  rating: string
-  myRating: string
+  rating?: string
   date?: dayjs.Dayjs | string
-  url: string
-  image: string
+  comment?: string
+  images: string[]
   location: string
 }
 
-const selectors = {
-  name: {
-    value: ".simple-rvw__rst-name-target",
-    fn: (item: Element) => item.textContent,
-  },
-  rating: {
-    value: ".simple-rvw__rate b.c-rating__val",
-    fn: (item: Element) => item.textContent,
-  },
-  myRating: {
-    value: ".p-preview-visit b.c-rating-v2__val",
-    fn: (item: Element) => item.textContent,
-  },
-  date: {
-    value: ".p-preview-visit__visited-date",
-    fn: (item: Element) => item.textContent,
-  },
-  url: {
-    value: ".simple-rvw__rst-name-target",
-    fn: (item: HTMLAnchorElement) => item.href,
-  },
-  image: {
-    value: ".simple-rvw__rst-img img",
-    fn: (item: HTMLImageElement) => item.src,
-  },
-  location: {
-    value: ".simple-rvw__area-catg",
-    fn: (item: Element) => item.textContent.trim(),
-  }
-}
+/**
+ * サイト内のShopデータを取得する
+ */
+const getShops = async (page: Page, url: string): Promise<Shop[]> => {
+  const postSelector = ".rvw-item__review-contents-wrap"
 
-const shops: Shop[] = []
+  await page.goto(`${SITE_URL}${url}`)
+  await page.waitForSelector(postSelector)
 
-const getShops = async (page: Page): Promise<Shop[]> => {
-  await page.waitForSelector(".rvw-item")
+  const name     = (await page.$eval(".rvw-item__rst-name", elm => elm.textContent))?.trim()
+  const location = (await page.$eval(".rvw-item__rst-area-catg", elm => elm.textContent))?.trim()
 
-  const $shops = (await page.$$(".rvw-item"))
+  const $posts = (await page.$$(postSelector))
 
-  const getInfoPromises = $shops.map(async $shop => {
-    const shop: Shop = {
-      name: "",
-      rating: "",
-      myRating: "",
-      date: null,
-      url: "",
-      image: "",
-      location: "",
-    }
-    const promises = Object.entries(selectors).map(async ([key, selector]: [string, {value: string, fn: (e: Element) => string}]) => {
-      (shop as any)[key] = await $shop.$eval(selector.value, selector.fn).catch((err) => console.error(err))
-      return shop
-    }, shop)
-
-    await Promise.all(promises)
-
-    return shop
-  })
-
-  const _shops = (await Promise.all(getInfoPromises))
-    .map(({name, date, rating, myRating, url, image, location}) => {
-      const _date = date != null ? (date as string).replace(/訪問/g, "") : null
-      const _name = name.trim()
+  const shops: Shop[] = await Promise.all(
+    $posts.map(async $post => {
+      const rating  = (await $post.$eval(".c-rating-v2__val", elm => elm.textContent).catch(err => {
+        console.error(err)
+        return ''
+      }))
+      const date    = (await $post.$eval(".rvw-item__visited-date", elm => elm.textContent).catch(err => {
+        console.error(err)
+        return ''
+      }))?.replace(/訪問/g, "").trim()
+      const images  = (await $post.$$eval(".rvw-photo__list-img img", elements => elements.map((elm: HTMLImageElement) => elm.src)).catch(err => {
+        console.error(err)
+        return []
+      }))
+      const comment = (await $post.$eval(".rvw-item__rvw-comment p", elm => elm.textContent).catch(err => {
+        console.error(err)
+        return ''
+      }))?.trim()
       return {
-        name: _name,
-        rating,
-        myRating,
-        date: _date && dayjs(_date).isValid ? dayjs(_date).toISOString() : null,
-        url,
-        image,
+        name,
         location,
+        rating,
+        comment,
+        images,
+        date: dayjs(date).isValid() ? dayjs(date).toISOString() : null,
       }
     })
+  )
 
-  if(_shops.length <= 0) return shops
-
-  shops.push(..._shops)
-
-  const $next = (await page.$$('a.c-pagination__arrow[rel="next"]'))
-  if($next.length <= 0) return shops
-  await $next[0].click()
-  return await getShops(page)
+  return shops
 }
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ headless: true })
   const page = (await browser.pages())[0]
   try {
-    const listPage = await auth(page)
-    const shops = await getShops(listPage)
-    await browser.close()
+    await auth(page)
 
-    fs.writeFileSync("output.json", JSON.stringify(shops, null, 4))
+    await page.goto("https://tabelog.com/rvwr/010252527/visited_restaurants/list/")
+
+    // const urls = await getPosts(page)
+    // fs.writeFileSync("urls.json", JSON.stringify(urls, null, 4))
+
+    const urls = JSON.parse(fs.readFileSync("urls.json", 'utf-8')) as string[]
+    const allShops: Shop[] = []
+    const chunked = chunk(urls, 20)
+    for await (const chunkUrls of chunked) {
+      const shops = await Promise.all(chunkUrls.map(async url => {
+        const p = await browser.newPage()
+        const _shops =  await getShops(p, url)
+        await p.close()
+        return _shops
+      }))
+      allShops.push(...shops.flat())
+    }
+
+    // const shops = (await Promise.all(urls.map(async url => await getShops(page, url)))).flat()
+    fs.writeFileSync("output.json", JSON.stringify(allShops, null, 4))
+
+    await browser.close()
   }
   catch(err) {
-    // await browser.close()
+    await browser.close()
     throw err
   }
 })();
