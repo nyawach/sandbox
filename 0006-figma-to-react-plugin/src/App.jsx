978d05f41bdec3e6cd43b2859f3356cea66c2d73
himeno-yusuke <himeno-yusuke@kayac.com>
2021-04-20 19:57:13 +0900

    create-react-app -> コンポーネント描画に挑戦

---
+++ 0006-figma-to-react-plugin/src/App.jsx
@@ -0,0 +1,77 @@
+import 'App.css'
+
+const Contacts = () => {
+  return (
+    <div className="contacts">
+      <div className="group- -1-3">
+        <div className="img" src="" />
+        <div className="img" src="" />
+        <div className="img" src="" />
+        <div className="contacts-\-4-line">
+          <div className="group- -1-2- -copy- -3">
+            <p className="text">Attachment</p>
+            <div className="contacts-\-4-line-\-attachment">
+              <div className="rectangle- -copy- -3-6" />
+              <div className="img" src="">
+            </div>
+            <p className="text">Time</p>
+            <p className="text">Name</p>
+            <div className="contacts-\-4-line-\-picture">
+              <div className="rectangle- -copy- -2-5" />
+              <p className="text">👴🏽</p>
+            </div>
+          </div>
+        </div>
+        <div className="contacts-\-3-line">
+          <div className="group- -1-2- -copy- -2">
+            <p className="text">Voice message</p>
+            <div className="contacts-\-3-line-\-recorder">
+              <div className="rectangle- -copy- -3-6" />
+              <div className="img" src="">
+            </div>
+            <p className="text">Time</p>
+            <p className="text">Name</p>
+            <div className="contacts-\-3-line-\-picture">
+              <div className="rectangle- -copy- -2-5" />
+              <p className="text">👱🏻‍♀️</p>
+            </div>
+          </div>
+        </div>
+        <div className="contacts-\-2-line">
+          <div className="group- -1-2- -copy">
+            <div className="contacts-\-2-line-\-meatballs-_-menu">
+              <div className="group- -1-4">
+                <div className="rectangle- -copy- -3-7" />
+                <div className="rectangle- -copy- -3-7" />
+              </div>
+            </div>
+            <p className="text">Text</p>
+            <p className="text">Text</p>
+            <p className="text">Name</p>
+            <div className="contacts-\-2-line-\-picture">
+              <div className="rectangle- -copy- -2-5" />
+              <p className="text">👨🏻</p>
+            </div>
+          </div>
+        </div>
+        <div className="contacts-\-1-line">
+          <div className="group- -1-2">
+            <div className="contacts-\-1-line-\-3">
+              <div className="rectangle- -copy- -3-6" />
+              <p className="text">3</p>
+            </div>
+            <p className="text">Time</p>
+            <p className="text">Text</p>
+            <p className="text">Name</p>
+            <div className="contacts-\-1-line-\-picture">
+              <div className="rectangle- -copy- -2-5" />
+              <p className="text">👩🏾</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Contacts
