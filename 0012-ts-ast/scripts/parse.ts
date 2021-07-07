3179737fa47f096b0296e0dab156daf54987f83d
himeno-yusuke <himeno-yusuke@kayac.com>
2021-07-07 18:42:09 +0900

    2020に変更

--- 0012-ts-ast/scripts/parse.ts
+++ 0012-ts-ast/scripts/parse.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs'
 
 ;(async () => {
   const source = fs.readFileSync(path.resolve(__dirname, '../stories/atoms/Badge.stories.ts'), 'utf-8')
-  const sourceFile = ts.createSourceFile('output.ts', source, ts.ScriptTarget.ES2015)
+  const sourceFile = ts.createSourceFile('output.ts', source, ts.ScriptTarget.ES2020)
   const unique = new Set()
   sourceFile.forEachChild(node => {
     unique.add(ts.SyntaxKind[node.kind])
