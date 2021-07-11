64af65d1261a5c52c8f2afe5347360edbd782f34
himeno-yusuke <himeno-yusuke@kayac.com>
2021-07-11 19:30:35 +0900

    nuxt ts stylus pug

---
+++ 0013-nuxt-vite/pages/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  Tutorial
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Tutorial from '../components/Tutorial.vue'
+
+export default Vue.extend({
+  components: {
+    Tutorial,
+  }
+})
+</script>
+
+<style lang="stylus" scoped>
+</style>
