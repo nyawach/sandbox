7e0fbebd5d7badbd577286934483c39f5f8e5fa8
himeno-yusuke <himeno-yusuke@kayac.com>
2021-07-18 16:16:42 +0900

    textに動的にデータを入れる

--- 0014-puppeteer-intl/pages/ogp/[text].tsx
+++ 0014-puppeteer-intl/pages/ogp/[text].tsx
@@ -1,9 +1,15 @@
+import { useRouter } from 'next/dist/client/router'
 import OgpLayout from '../../components/OgpLayout'
 
-const IndexPage = () => (
-  <OgpLayout title="Home | Next.js + TypeScript Example">
-    <h1>Home | Next.js + TypeScript Example</h1>
-  </OgpLayout>
-)
+const IndexPage = () => {
+  const router = useRouter()
+  const text = router.query.text
+  if (text instanceof Array) return null
+  return (
+    <OgpLayout title={text}>
+      <h1>{text}</h1>
+    </OgpLayout>
+  )
+}
 
 export default IndexPage
