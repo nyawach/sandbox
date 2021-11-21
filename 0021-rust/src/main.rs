b255cc2c7471e45d4600158e7fd6e0db0c68ab0c
himeno-yusuke <himeno-yusuke@kayac.com>
2021-11-21 14:15:58 +0900

    pattern match便利

--- 0021-rust/src/main.rs
+++ 0021-rust/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    for n in 1..40 {
+        fizzbuzz(n);
+    }
+}
+
+fn fizzbuzz(n: i32) {
+    match (n % 3, n % 5) {
+        (0, 0) => println!("FizzBuzz"), // nを3で割った余りと5で割った余りが両方0
+        (0, _) => println!("Fizz"),     // nを3で割った余りが0
+        (_, 0) => println!("Buzz"),     // nを5で割った余りが0
+        _      => println!("{}", n),
+    }
 }
