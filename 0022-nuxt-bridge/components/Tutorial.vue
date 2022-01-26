b2a3a751f0583354f10fb2abff0be2825c30e21f
nyawach <yuunyawach@gmail.com>
2022-01-26 12:50:52 +0900

    0022: nuxt2+@vue/composition-apiのベースを作る

---
+++ 0022-nuxt-bridge/components/Tutorial.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'Tutorial',
+  setup() {
+    const text = ref('Hello!')
+    return {
+      text,
+    }
+  }
+})
+</script>
---
+++ 0022-nuxt-bridge/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Tutorial from '~/components/Tutorial.vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  components: {
+    Tutorial,
+  },
+})
+</script>
