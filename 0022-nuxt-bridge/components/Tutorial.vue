e1a5d0dfa2d5c616a4bb97fcdbf5b78fdc3dadf7
nyawach <yuunyawach@gmail.com>
2022-01-28 14:24:58 +0900

    use i18n component & root.$i18n

--- 0022-nuxt-bridge/components/Tutorial.vue
+++ 0022-nuxt-bridge/components/Tutorial.vue
@@ -1,17 +1,31 @@
 <template>
-  <h1>{{ text }}</h1>
+  <div>
+    <h1>{{ text }}</h1>
+    <h2>
+      <i18n path="Hello {world}!">
+        <template v-slot:world>
+          <b>{{ $t('World') }}</b>
+        </template>
+      </i18n>
+    </h2>
+    <select v-model="lang">
+      <option value="ja">ja</option>
+      <option value="en">en</option>
+    </select>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@vue/composition-api'
+import { defineComponent, ref, watch } from '@vue/composition-api'
 
 export default defineComponent({
   name: 'Tutorial',
-  setup() {
-    const text = ref('Hello!')
-    return {
-      text,
-    }
+  setup(_, context) {
+    const lang = ref(context.root.$i18n.locale)
+    watch(lang, newLang => {
+      context.root.$i18n.setLocale(newLang)
+    })
+    return { lang }
   }
 })
 </script>
