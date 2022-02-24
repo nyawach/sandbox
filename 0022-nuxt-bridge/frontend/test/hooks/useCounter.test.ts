e25779ffe153d750d0621ad39a2fc1f1d263b000
nyawach <yuunyawach@gmail.com>
2022-02-24 11:55:30 +0900

    coverage 100% にする

--- 0022-nuxt-bridge/frontend/test/hooks/useCounter.test.ts
+++ 0022-nuxt-bridge/frontend/test/hooks/useCounter.test.ts
@@ -6,7 +6,30 @@ describe('useCounter.ts', () => {
         const { count } = useCounter(v)
         expect(count.value).toBe(v)
     })
-    it('incrementで1上がる', () => {
-        
+    describe('increment', () => {
+        it('incrementで1上がる', () => {
+            const { count, increment } = useCounter()
+            increment()
+            expect(count.value).toBe(1)
+        })
+        it('incrementで初期値+1上がる', () => {
+            const v = 100
+            const { count, increment } = useCounter(v)
+            increment()
+            expect(count.value).toBe(v + 1)
+        })
+    })
+    describe('decrement', () => {
+        it('decrementで1下がる', () => {
+            const { count, decrement } = useCounter()
+            decrement()
+            expect(count.value).toBe(-1)
+        })
+        it('decrementで初期値+1下がる', () => {
+            const v = 100
+            const { count, decrement } = useCounter(v)
+            decrement()
+            expect(count.value).toBe(v - 1)
+        })
     })
 })
