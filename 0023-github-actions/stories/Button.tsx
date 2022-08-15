f440a1936f9d6c98ab4743403394f75a1d6be903
himeno-yusuke <himeno-yusuke@kayac.com>
2022-08-15 23:42:52 +0900

    remove comments etc.

--- 0023-github-actions/stories/Button.tsx
+++ 0023-github-actions/stories/Button.tsx
@@ -1,39 +1,21 @@
 import React from 'react';
 import './button.css';
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
+type Props = {
   primary?: boolean;
-  /**
-   * What background color to use
-   */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
   primary = false,
   size = 'medium',
   backgroundColor,
   label,
   ...props
-}: ButtonProps) => {
+}: Props) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
