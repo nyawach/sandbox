66332d12aa106453f640afa41d25a9e447f69498
nyawach <yuunyawach@gmail.com>
2022-09-25 23:27:04 +0900

    lint/prettier

---
+++ 0026-vite-react/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react'
+  ],
+  rules: {
+  },
+}
