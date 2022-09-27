2f0e65c85493e14683e75975560b2aee6da1a42c
himeno-yusuke <himeno-yusuke@kayac.com>
2022-09-27 17:52:54 +0900

    Homeをlazy importする

--- 0026-vite-react/src/components/ApplicationRoutes.tsx
+++ 0026-vite-react/src/components/ApplicationRoutes.tsx
@@ -1,17 +1,25 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import DefaultLayout from "./layouts/DefaultLayout";
-import Home from "./templates/Home";
-import NotFound from "./templates/NotFound";
+import DefaultLayout from "~/components/layouts/DefaultLayout";
+import NotFound from "~/components/templates/NotFound";
 
-const ApplicationRoutes = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route index element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-)
+const Home = lazy(() => import('~/components/templates/Home'))
+
+const ApplicationRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route index element={
+            <Suspense fallback={<>Loading...</>}>
+              <Home />
+            </Suspense>
+          } />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default ApplicationRoutes
