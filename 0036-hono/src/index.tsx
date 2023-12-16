ac34327fdc28a57a648efa72884032718dc0eb7c
nyawach <yuunyawach@gmail.com>
2023-12-16 13:54:46 +0900

    add /button

--- 0036-hono/src/index.tsx
+++ 0036-hono/src/index.tsx
@@ -3,5 +3,6 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => c.text('Hello Hono!'))
+app.get('/button', (c) => c.render(<button>{c.status}</button>))
 
 export default app
