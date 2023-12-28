e70d9791f431c0bb6ae197df6e99df883a4eaca8
nyawach <himeno.yusuke@moneyforward.co.jp>
2023-12-28 16:52:58 +0900

    Hono + Deno + OpenAI

--- 0037-deno-openai/index.ts
+++ 0037-deno-openai/index.ts
@@ -1,19 +1,33 @@
 import OpenAI from "npm:openai";
+import { Hono } from "npm:hono";
+import { zValidator } from "npm:@hono/zod-validator";
+import { z } from "npm:zod";
 import { load } from "https://deno.land/std@0.210.0/dotenv/mod.ts";
 
-const env = await load();
+const app = new Hono();
+
+await load();
 
 const openai = new OpenAI({
   apiKey: Deno.env.get("OPENAI_API_KEY"),
 });
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
+app.get(
+  "/api/talk",
+  zValidator(
+    "query",
+    z.object({
+      message: z.string(),
+    })
+  ),
+  async (c) => {
+    const message = c.req.query("message") ?? "No message for you.";
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: message }],
+      model: "gpt-3.5-turbo",
+    });
+    return c.json(completion.choices);
+  }
+);
 
-  console.log(completion.choices[0]);
-}
-
-main();
+Deno.serve(app.fetch);
