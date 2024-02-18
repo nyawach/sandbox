03e99cbbfc8a415f46fd1fa9384402e5ee0e8f86
nyawach <yuunyawach@gmail.com>
2024-02-18 15:31:08 +0900

    add /stream

--- 0036-hono/src/index.tsx
+++ 0036-hono/src/index.tsx
@@ -1,8 +1,19 @@
 import { Hono } from 'hono'
+import { streamText } from "hono/streaming";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Hono!'))
-app.get('/button', (c) => c.render(<button>{c.status}</button>))
+app.get('/', (c) => c.text('Hello Hono!')) 
+
+app.get("/stream", (c) => {
+  return streamText(c, async (stream) => {
+    // Write a text with a new line ('\n').
+    await stream.writeln("Hello");
+    // Wait 1 second.
+    await stream.sleep(1000);
+    // Write a text without a new line.
+    await stream.write(`Hono!`);
+  });
+});
 
 export default app
